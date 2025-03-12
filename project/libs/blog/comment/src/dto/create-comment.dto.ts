5d557926bd00badaa72875097ef1fbad646b8790
Anatoly Dolgov <anatoly.dolgov@uchi.ru>
2025-03-12 21:47:45 +0300

    feat: enhance Comment DTOs with Swagger decorators and add API responses to CommentController methods

--- project/libs/blog/comment/src/dto/create-comment.dto.ts
+++ project/libs/blog/comment/src/dto/create-comment.dto.ts
@@ -1,7 +1,22 @@
 import { Comment } from '@avylando-readme/core';
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 class CreateCommentDto implements Pick<Comment, 'authorId' | 'content'> {
+  @ApiProperty({
+    description: 'Comment author ID',
+    type: 'string',
+    example: '60f5b2b3c4e9d2b9c8b2c8b2c',
+  })
+  @Expose()
   public authorId: Comment['authorId'];
+
+  @ApiProperty({
+    description: 'Comment content',
+    type: 'string',
+    example: 'Comment content',
+  })
+  @Expose()
   public content: Comment['content'];
 }
 
--- project/libs/blog/comment/src/dto/update-comment.dto.ts
+++ project/libs/blog/comment/src/dto/update-comment.dto.ts
@@ -1,6 +1,14 @@
 import { Comment } from '@avylando-readme/core';
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 class UpdateCommentDto implements Pick<Comment, 'content'> {
+  @ApiProperty({
+    description: 'Comment content',
+    type: 'string',
+    example: 'Comment content',
+  })
+  @Expose()
   public content: Comment['content'];
 }
 
