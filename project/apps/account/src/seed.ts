5f602373fca4c008cbf3c73bf050a7605af48da5
Anatoly Dolgov <anatoly.dolgov@uchi.ru>
2025-03-24 14:09:43 +0300

    feat: add database seeding functionality and update TypeScript configuration

---
+++ project/apps/account/src/seed.ts
@@ -0,0 +1,50 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthenticationService } from '@project/authentication';
+import { AppModule } from './app/app.module';
+
+function getUsers() {
+  return [
+    {
+      email: 'example@mail.com',
+      password: '123456',
+      firstName: 'Иван',
+      lastName: 'Иванов',
+      avatarSrc: 'https://example.com/avatar.jpg',
+    },
+    {
+      email: 'user@mail.cu',
+      password: '12345',
+      firstName: 'Петр',
+      lastName: 'Петров',
+      avatarSrc: 'https://example.com/avatar.jpg',
+    },
+  ];
+}
+
+async function seedDb(authenticationService: AuthenticationService) {
+  const users = getUsers();
+  for (const user of users) {
+    await authenticationService.register(user);
+  }
+
+  console.log('Users seeded');
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: console,
+    bufferLogs: true,
+  });
+  const service = app.get(AuthenticationService);
+  if (service) {
+    await seedDb(service);
+    await app.close();
+    process.exit(0);
+  } else {
+    console.error('AuthenticationService not found');
+    await app.close();
+    process.exit(1);
+  }
+}
+
+bootstrap();
